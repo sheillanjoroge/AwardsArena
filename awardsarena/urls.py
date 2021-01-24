54b25f5be6f1e58625825f872aedd9cd5dbba0a9
sheillanjoroge <sheillan.njoroge@gmail.com>
2021-01-25 01:19:50 +0300

    add url conf

--- awardsarena/urls.py
+++ awardsarena/urls.py
@@ -3,11 +3,11 @@ from django.contrib import admin
 from django.conf.urls import url, include
 from awards.views import loginPage, registerPage, show_post
 
-urlpatterns = [
-    url('admin/', admin.site.urls),
-    url(r'^register', registerPage, name='register'),
-    url(r'^login', loginPage, name='login'),
-    url('', include('awards.urls') ), 
+# urlpatterns = [
+#     url('admin/', admin.site.urls),
+#     url(r'^register', registerPage, name='register'),
+#     url(r'^login', loginPage, name='login'),
+#     url('', include('awards.urls') ), 
     url(r'^profile', include('awards.urls')),
     url(r'^upload', include('awards.urls')),
     url(r'^post/(\d)/', include('awards.urls')),
