dde022466ac6fbae027c1423d199a5c2d49b7e14
sheillanjoroge <sheillan.njoroge@gmail.com>
2021-01-25 01:18:06 +0300

    add and edit conf

--- awardsarena/settings.py
+++ awardsarena/settings.py
@@ -68,12 +68,12 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'awards', 
-    'rest_framework',
+    # 'django.contrib.contenttypes',
+    # 'django.contrib.sessions',
+    # 'django.contrib.messages',
+    # 'django.contrib.staticfiles',
+    # 'awards', 
+    # 'rest_framework',
 ]
 
 MIDDLEWARE = [
