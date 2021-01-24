2cccf78f1a107ad6c8f5d890a4f8200750123585
sheillanjoroge <sheillan.njoroge@gmail.com>
2021-01-25 01:44:15 +0300

    add more urls conf for the profile page

--- awards/urls.py
+++ awards/urls.py
@@ -5,11 +5,11 @@ from django.conf.urls.static import static
 from awards.views import index, profile, upload_pic, show_post, vote_post, UsersList, ProjectsList
 #follow_request, like_post, make_comment
 
-urlpatterns = [
-    url(r'^$', index, name='index'),
-    url(r'^profile', profile, name='profile'),
-    url(r'^upload', upload_pic, name='upload'),
-    url(r'^post/(\d)/', show_post, name='post'),
+# urlpatterns = [
+#     url(r'^$', index, name='index'),
+#     url(r'^profile', profile, name='profile'),
+#     url(r'^upload', upload_pic, name='upload'),
+#     url(r'^post/(\d)/', show_post, name='post'),
     # url(r'^follow$', follow_request, name='follow' ),
     url(r'^vote/(\d)/', vote_post, name='vote' ),
     url(r'^api/users/$', UsersList.as_view()),
