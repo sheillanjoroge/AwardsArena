5893464c8258a1eb6bbc5d7c060ada4c54ce906c
sheillanjoroge <sheillan.njoroge@gmail.com>
2021-01-25 01:32:18 +0300

    add  administrative tasks to the admin

--- awards/admin.py
+++ awards/admin.py
@@ -1,7 +1,7 @@
-# from django.contrib import admin
-# from .models import Profile, Post, Rate, Comment
+from django.contrib import admin
+from .models import Profile, Post, Rate, Comment
 
-# admin.site.register(Profile)
-# admin.site.register(Post)
-# admin.site.register(Rate)
-# admin.site.register(Comment)
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Rate)
+admin.site.register(Comment)
