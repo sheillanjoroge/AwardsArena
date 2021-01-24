c32da836b5fd07390c7e09bc68345cae9bab232d
sheillanjoroge <sheillan.njoroge@gmail.com>
2021-01-25 01:23:44 +0300

    add imports

--- awardsarena/wsgi.py
+++ awardsarena/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for awardsarena project.
+WSGI config for Desiiigner project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'awardsarena.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Desiiigner.settings')
 
 application = get_wsgi_application()
