195903908698a2bfe2f91335a1a5e62fe300c72b
sheillanjoroge <sheillan.njoroge@gmail.com>
2021-01-25 01:38:11 +0300

    add more classes and models for successive functionality

--- awards/models.py
+++ awards/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-from django.dispatch import receiver
-from django.db.models.signals import post_save
+# from django.dispatch import receiver
+# from django.db.models.signals import post_save
 
-User =  get_user_model()
+# User =  get_user_model()
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_image = models.ImageField(upload_to = 'profiles/')
+# class Profile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     profile_image = models.ImageField(upload_to = 'profiles/')
 
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
