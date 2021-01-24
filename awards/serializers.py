60fbc43c4e70fd0c794a3a4fdc016b8e0595686e
sheillanjoroge <sheillan.njoroge@gmail.com>
2021-01-25 01:40:50 +0300

    add more function and serializers

--- awards/serializers.py
+++ awards/serializers.py
@@ -4,13 +4,13 @@ from .models import Post
 
 User = get_user_model()
 
-# class UsersSerialized(serializers.ModelSerializer):
-#     class Meta:
-#         model = User
-#         fields = ('id', 'username', 'email')
+class UsersSerialized(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email')
 
 
-# class ProjectsSerialized(serializers.ModelSerializer):
-#     class Meta:
+class ProjectsSerialized(serializers.ModelSerializer):
+    class Meta:
         model = Post
         fields = ('id', 'post_image', 'post_description', 'user', 'upload_date')
